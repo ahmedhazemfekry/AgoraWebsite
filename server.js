7c0d0f1fa34c234d0ed724d62186b16fef6a5586
ahmed hazem <a.hazem1995@gmail.com>
2018-07-18 16:06:14 +0200

    intialization

---
+++ server.js
@@ -0,0 +1,20 @@
+// server.js
+
+const express = require('express');
+const app = express();
+const port = 3000;
+const CoinRouter = require('./routes/CoinRouter');
+
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.use('/coins', CoinRouter);
+
+app.get('/', function (req, res) {
+   res.sendFile(path.join(__dirname,'public', 'index.html'));
+});
+
+app.listen(port, function(){
+  console.log('Node js Express js Tutorial');
+});
